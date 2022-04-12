89c14ef17916f48b5fc5953af4d1010c40d4e0a7
Bisrat Kebere ALx <kiyyakebere@gmail.com>
2022-04-12 05:14:07 -0700

    Added for 0x02

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,24 +13,24 @@ void times_table(void)
 	{
 		for (l = 0; l < 10; l++)
 		{
-		    a = j * l;
-		    if ((a / 10) == 0)
-		    {
-		        _putchar((a % 10) + '0');
-		    }
-		    else
-		    {
-    			_putchar((a / 10) + '0');
-    			_putchar((a % 10) + '0');
-		    }
+			a = j * l;
+		    	if ((a / 10) == 0)
+		    	{
+		        	_putchar((a % 10) + '0');
+		    	}
+		    	else
+		    	{
+    				_putchar((a / 10) + '0');
+    				_putchar((a % 10) + '0');
+		    	}
 			if (l != 9)
 			{
-			    _putchar(',');
-			    _putchar(' ');
-			    if ((a / 10) == 0)
-			    {
-			        _putchar(' ');
-			    }
+				_putchar(',');
+				_putchar(' ');
+				if ((a / 10) == 0)
+				{
+				_putchar(' ');
+				}
 			}
 		}
 		_putchar('\n');
